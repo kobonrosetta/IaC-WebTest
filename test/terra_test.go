e1e63732a7a0262d34ff560f276ce9fce38327ec
kobonrosetta <kobonrosetta@gmail.com>
2020-10-09 00:50:46 -0700

    test4

--- test/terra_test.go
+++ test/terra_test.go
@@ -8,15 +8,6 @@ import (
   http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 
   "github.com/gruntwork-io/terratest/modules/terraform"
-  // added beta testing
-  "os"
-
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/service/ec2"
-  "github.com/gruntwork-io/terratest/modules/collections"
-  "github.com/gruntwork-io/terratest/modules/logger"
-  "github.com/gruntwork-io/terratest/modules/random"
-  "github.com/gruntwork-io/terratest/modules/testing"
 )
 
 func TestTerraformAwsHelloWorldExample(t *testing.T) {
@@ -35,8 +26,4 @@ func TestTerraformAwsHelloWorldExample(t *testing.T) {
 
   url := fmt.Sprintf("http://%s:8080", publicIp)
   http_helper.HttpGetWithRetry(t, url, nil, 200, "Hello, World!", 30, 5*time.Second)
-
-  // testing beta testing below this
-
-
 }
