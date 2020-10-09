b1e15fc1f90e42acb2ac3c07c4063bb29b2ab39c
kobonrosetta <kobonrosetta@gmail.com>
2020-10-09 01:18:06 -0700

    test4

--- test/terra_test.go
+++ test/terra_test.go
@@ -1,4 +1,4 @@
-package test
+package test, aws
 
 import (
   "fmt"
@@ -8,7 +8,41 @@ import (
   http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 
   "github.com/gruntwork-io/terratest/modules/terraform"
+
+  "os"
+
+  "github.com/aws/aws-sdk-go/aws"
+  "github.com/aws/aws-sdk-go/service/ec2"
+  "github.com/gruntwork-io/terratest/modules/collections"
+  "github.com/gruntwork-io/terratest/modules/logger"
+  "github.com/gruntwork-io/terratest/modules/random"
+  "github.com/gruntwork-io/terratest/modules/testing"
+  "github.com/stretchr/testify/assert"
 )
+//////////////
+
+func GetRandomRegion(t testing.TestingT, approvedRegions []string, forbiddenRegions []string) string {
+	region, err := GetRandomRegionE(t, approvedRegions, forbiddenRegions)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return region
+}
+
+func TestGetRandomRegionExcludesForbiddenRegions(t *testing.T) {
+	t.Parallel()
+
+	approvedRegions := []string{"us-east-2"}
+	forbiddenRegions := []string{"us-west-2", "ap-northeast-2", "ca-central-1", "us-east-1", "us-west-1", "us-west-2", "eu-west-1", "eu-west-2", "eu-central-1", "ap-southeast-1", "ap-northeast-1", "ap-northeast-2", "ap-south-1""}
+
+	for i := 0; i < 1000; i++ {
+		randomRegion := GetRandomRegion(t, approvedRegions, forbiddenRegions)
+		assert.NotContains(t, forbiddenRegions, randomRegion)
+	}
+}
+
+
+///////////
 
 func TestTerraformAwsHelloWorldExample(t *testing.T) {
   t.Parallel()
